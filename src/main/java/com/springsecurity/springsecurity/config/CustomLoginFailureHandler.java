6cd9a17540e208d9c6f6a0f012e1ef1be0933f1d
didgjstn585 <com6474@gmail.com>
2023-06-23 21:08:04 +0900

    fix : 로그인 검증 추가
    - 아이디 존재 여부 검증

--- src/main/java/com/springsecurity/springsecurity/config/CustomLoginFailureHandler.java
+++ src/main/java/com/springsecurity/springsecurity/config/CustomLoginFailureHandler.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
 import java.io.IOException;
@@ -22,6 +23,9 @@ public class CustomLoginFailureHandler implements AuthenticationFailureHandler {
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response
             , AuthenticationException exception) throws IOException, ServletException {
 
+        if (exception instanceof UsernameNotFoundException) {
+            response.sendRedirect("/login/fail");
+        }
         if (exception instanceof BadCredentialsException) {
             response.sendRedirect("/login/fail");
         } else if (sessionRepository2.existsByUserId(request.getParameter("userId"))) {
